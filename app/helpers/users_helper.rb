ea907b87735c52f6d7239eb431c8417f32d629d7
wuhlcom <378433855@qq.com>
2016-09-30 17:29:51 +0800

    Temp commit before 10 1

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+ # 返回指定用户的 Gravatar
+   def gravatar_for(user,args={size:80})
+     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+     size=args[:size]
+     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+     image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    
+    # "https://s.gravatar.com/avatar/add6d3c82f3610122dc46a4cabcb2a5b?s=80"
+    # gravatar_url = "https://s.gravatar.com/avatar/#{gravatar_id}?s=80"
+    # image_tag(gravatar_url, alt: user.name, class: "gravatar",size:args[:size])
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	before_save{self.email=email.downcase}
         VALID_EMAIL_REGEX =  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	
 	validates :name, presence: true, length: {minimum:6, maximum: 50 }
 	validates :email, presence: true, length: {minimum:4, maximum: 255 },
  		         uniqueness:{case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
