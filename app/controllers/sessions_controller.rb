be7d4086ef216573a6c474f62b8df664eaa4808f
wuhlcom <378433855@qq.com>
2016-10-11 15:16:51 +0800

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-	 user = User.find_by(email: params[:session][:email].downcase)
-	if user && user.authenticate(params[:session][:password])
-		log_in user   # 登入用户，然后重定向到用户的资料页面
-		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-		redirect_to user
+	 @user = User.find_by(email: params[:session][:email].downcase)
+	if @user && @user.authenticate(params[:session][:password])
+		log_in @user   # 登入用户，然后重定向到用户的资料页面
+		params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+		redirect_to @user
         else
 	 	# 创建一个错误消息
 		 flash.now[:danger] = 'Invalid email/password combination'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -39,4 +39,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 	assert_select "a[href=?]", user_path(@user),count: 0
 	
    end
+  
+   test "login with remembering" do
+	log_in_as(@user, remember_me: '1')
+	assert_equal User.digest(cookies['remember_token']),assigns(:user).remember_digest)
+	assert_not_nil cookies['remember_token']
+   end
+
+   test "login without remembering" do
+	log_in_as(@user, remember_me: '0')
+	assert_nil cookies['remember_token']
+    end
+
 end
