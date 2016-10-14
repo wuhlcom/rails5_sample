04bced27a1eab6f5dd8cb9376314850088e9ceda
wuhlcom <378433855@qq.com>
2016-10-14 18:01:08 +0800

    password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token,:activation_token
+	attr_accessor :remember_token,:activation_token,:reset_token
 	#before_save{self.email=email.downcase}
 	before_save :downcase_email
         before_create :create_activation_digest
@@ -16,6 +16,7 @@ class User < ApplicationRecord
        def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
+		#"$2a$10$LHtnMk/mbWgRauQfG2reH.UhEAyGiwgMMEc5N/gSwk4V1FzApzr9S"
        end
 
        def User.new_token
@@ -46,7 +47,21 @@ class User < ApplicationRecord
     def send_activation_email
            UserMailer.account_activation(self).deliver_now
     end
+
+    def create_reset_digest
+	self.reset_token= User.new_token
+       	update_columns(reset_digest:User.digest(reset_token),reset_sent_at:Time.zone.now)
+   end
                           
+    # Sends activation email.
+    def send_password_reset_email
+           UserMailer.password_reset(self).deliver_now
+    end
+
+    def password_reset_expired?
+	reset_sent_at < 2.hours.ago
+    end
+
   private   
        def downcase_email
 	 	self.email.downcase!
