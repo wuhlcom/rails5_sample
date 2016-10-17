47d54fb363eb1f5dc4d73f1d7072043e9cc1de2b
wuhlcom <378433855@qq.com>
2016-10-17 15:46:19 +0800

    Add password reset

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -26,37 +26,52 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
 	 # 电子邮件地址错误
 	 get edit_password_reset_path(user.reset_token, email: "")
 	 assert_redirected_to root_url
- # 用户未激活
- user.toggle!(:activated)
- get edit_password_reset_path(user.reset_token, email: user.email)
- assert_redirected_to root_url
- user.toggle!(:activated)
- # 电子邮件地址正确，令牌不对
- get edit_password_reset_path('wrong token', email: user.email)
- assert_redirected_to root_url
-# 电子邮件地址正确，令牌也对
- get edit_password_reset_path(user.reset_token, email: user.email)
- assert_template 'password_resets/edit'
- assert_select "input[name=email][type=hidden][value=?]", user.email
- # 密码和密码确认不匹配
- patch password_reset_path(user.reset_token),
- params: { email: user.email,
- user: { password: "foobaz",
- password_confirmation: "barquux" } }
- assert_select 'div#error_explanation'
- # 密码为空值
- patch password_reset_path(user.reset_token),
- params: { email: user.email,
- user: { password: "",
- password_confirmation: "" } }
- assert_select 'div#error_explanation'
- # 密码和密码确认有效
- patch password_reset_path(user.reset_token),
- params: { email: user.email,
- user: { password: "foobaz",
- password_confirmation: "foobaz" } }
- assert is_logged_in?
- assert_not flash.empty?
- assert_redirected_to user
- end
+	 # 用户未激活
+	 user.toggle!(:activated)
+	 get edit_password_reset_path(user.reset_token, email: user.email)
+	 assert_redirected_to root_url
+	 user.toggle!(:activated)
+	 # 电子邮件地址正确，令牌不对
+	 get edit_password_reset_path('wrong token', email: user.email)
+	 assert_redirected_to root_url
+	# 电子邮件地址正确，令牌也对
+	 get edit_password_reset_path(user.reset_token, email: user.email)
+	 assert_template 'password_resets/edit'
+	 assert_select "input[name=email][type=hidden][value=?]", user.email
+	 # 密码和密码确认不匹配
+	 patch password_reset_path(user.reset_token),
+	 params: { email: user.email,
+		 user: { password: "foobaz",
+			 password_confirmation: "barquux" } }
+	 assert_select 'div#error_explanation'
+	 # 密码为空值
+	 patch password_reset_path(user.reset_token),
+	 params: { email: user.email,
+	 user: { password: "",
+	 password_confirmation: "" } }
+	 assert_select 'div#error_explanation'
+	 # 密码和密码确认有效
+	 patch password_reset_path(user.reset_token),
+	 params: { email: user.email,
+	 user: { password: "foobaz",
+	 password_confirmation: "foobaz" } }
+	 assert is_logged_in?
+	 assert_not flash.empty?
+	 assert_redirected_to user
+  end
+
+  test "expired token" do
+	get new_password_reset_path
+	post password_resets_path,
+	     params: { password_reset: { email: @user.email } }
+
+	@user = assigns(:user)
+	@user.update_attribute(:reset_sent_at, 3.hours.ago)
+	patch password_reset_path(@user.reset_token),
+	      params: { email: @user.email,user: { password: "foobar",password_confirmation: "foobar" } }
+
+	assert_response :redirect
+	follow_redirect!
+	assert_match /expired/i, response.body
+  end
 end
