610478816376f3fed9cee0a8a423cc71496e0e1f
wuhlcom <378433855@qq.com>
2016-10-18 19:42:00 +0800

    code 13.70

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'coffee-rails', '4.2.1'
 gem 'jquery-rails', '4.1.1'
 gem 'turbolinks', '5.0.1'
 gem 'jbuilder', '2.4.1'
+gem 'carrierwave', '0.11.2'
+gem 'mini_magick', '4.5.1'
+gem 'fog', '1.38.0'
 
 group :development, :test do
   gem 'byebug','9.0.0', platform: :mri
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show 
      @user=User.find(params[:id])
+     @microposts = @user.microposts.paginate(page:params[:page])
      redirect_to root_url and return unless @user.activated?
     # debugger
   end
@@ -58,14 +59,6 @@ class UsersController < ApplicationController
 		:password_confirmation)
 	end
 	
-        def logged_in_user
-		unless logged_in?
-			store_location
-			flash[:danger] = "Please log in."
-			redirect_to login_url
-		end
-	end
-
 	def correct_user
 		@user = User.find(params[:id])
 		redirect_to(root_url) unless current_user?(@user)
