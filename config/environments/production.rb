ed36e01a75a763cf34ed1ad82bc7f33cda23ac6a
wuhlcom <378433855@qq.com>
2016-10-26 15:05:39 +0800

    'Rails.production email'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,17 +84,23 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.raise_delivery_errors = true
-
   config.action_mailer.delivery_method = :smtp
-	host = 'wuhongliang.herokuapp.com'
-	config.action_mailer.default_url_options = { host: host }
-	ActionMailer::Base.smtp_settings = {
-		:address => 'smtp.sendgrid.net',
+  host = 'wuhongliang.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+		#:address => 'smtp.sendgrid.net',
+		#:port => '587',
+		#:authentication => :plain,
+		#:user_name => ENV['SENDGRID_USERNAME'],
+		#:password => ENV['SENDGRID_PASSWORD'],
+		#:domain => 'heroku.com',
+		#:enable_starttls_auto => true
+		:address => 'smtp.qq.com',
+		:domain => 'qq.com',
 		:port => '587',
 		:authentication => :plain,
-		:user_name => ENV['SENDGRID_USERNAME'],
-		:password => ENV['SENDGRID_PASSWORD'],
-		:domain => 'heroku.com',
+		:user_name =>'wuhlcom@qq.com',
+		:password => 'zjxnzfigishzbhba',
 		:enable_starttls_auto => true
   }
 end
