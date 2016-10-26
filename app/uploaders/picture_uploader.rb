31fa99ddce7fc5417d28eadb24aeb6b691c6e9df
wuhlcom <378433855@qq.com>
2016-10-26 14:53:47 +0800

    comments amazon s3

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,11 +7,11 @@ class PictureUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
    process resize_to_limit:[400,400]
 
-  if Rails.env.production?
-	storage :fog
-  else
+#  if Rails.env.production?
+#	storage :fog
+#  else
 	storage :file
-  end
+ # end
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
